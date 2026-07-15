ae1d99bde9b0ba83ebd1679f1df9d99a41a3a44b
Patrick Tran (Pat-GPT) <67556421+patricktran1@users.noreply.github.com>
2026-07-15 00:12:11 -0700

    Load GPS context styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import './release05.css';
 import './release06.css';
 import './release07.css';
 import './release08.css';
+import './release09.css';
 import { AddressBookPage } from './components/AddressBookPage';
 import { CampaignsPage } from './components/CampaignsPage';
 import { CampaignComposer } from './components/CampaignComposer';
