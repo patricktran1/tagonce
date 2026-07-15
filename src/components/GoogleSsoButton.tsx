1f136c86c8ae7a57bb4c4067740afd2843f09a5d
Patrick Tran (Pat-GPT) <67556421+patricktran1@users.noreply.github.com>
2026-07-15 13:27:32 -0700

    Load Google SSO connection styles

--- src/components/GoogleSsoButton.tsx
+++ src/components/GoogleSsoButton.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import '../release12.css';
 import { rememberGoogleCalendarReturn } from '../lib/calendarService';
 
 type GoogleIdentityApi = {
