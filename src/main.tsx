23fecbabc1b00888b6e08ff9f1dea48e78df186c
Patrick Tran (Pat-GPT) <67556421+patricktran1@users.noreply.github.com>
2026-07-17 12:07:11 -0700

    Register offline shell and connection feedback

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { NetworkStatus } from './components/NetworkStatus'
 import { prepareProductPolish } from './lib/productPolish'
+import { registerTagOnceServiceWorker } from './lib/pwa'
 
 prepareProductPolish()
+registerTagOnceServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <NetworkStatus />
   </StrictMode>,
 )
